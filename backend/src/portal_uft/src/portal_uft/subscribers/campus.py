368914a42c6841b642ba7fb35889035a8d09404f
Paulo Rogerio <paulorogerio.to@gmail.com>
2022-06-24 17:12:01 -0300

    Exercício 24

--- backend/src/portal_uft/src/portal_uft/subscribers/campus.py
+++ backend/src/portal_uft/src/portal_uft/subscribers/campus.py
@@ -1,5 +1,7 @@
+from kitconcept import api
 from portal_uft.content.campus import Campus
 from zope.lifecycleevent import ObjectAddedEvent
+from zope.lifecycleevent import ObjectModifiedEvent
 
 
 def _update_tags(obj: Campus):
@@ -8,6 +10,11 @@ def _update_tags(obj: Campus):
     city = obj.city
     tags.add(f"Campus: {city}")
     obj.subject = tuple(tags)
+
+
+def modified(obj: Campus, event: ObjectModifiedEvent):
+    """Post modification handler for Campus."""
+    _update_tags(obj)
 
 
 def added(obj: Campus, event: ObjectAddedEvent):
