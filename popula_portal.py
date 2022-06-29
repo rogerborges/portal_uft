ee9e5280b436fb4a4343fdb9fef845d37b0103d4
Paulo Rogerio <paulorogerio.to@gmail.com>
2022-06-29 15:33:15 -0300

    Exercício 45

--- popula_portal.py
+++ popula_portal.py
@@ -1,24 +1,17 @@
-import json
 import requests
-import sys
-
 
 base_url = "http://localhost:8080/Plone/++api++"
 username = "admin"
 password = "admin"
 
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
+headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
 # Authenticate
-response = requests.post(f"{base_url}/@login", headers=headers, json={"login": username, "password": password})
-
-# Create Campus
-responseCreate = requests.post(f"{base_url}/", headers={'Accept': 'application/json', 'Content-Type': 'application/json'},
-                               json={'@type': 'Document', 'id': 'campus4',  'title': 'Campus UFT', 'description': 'Lista de Campus da UFT'},
-                               auth=('admin', 'admin'))
+response = requests.post(
+    f"{base_url}/@login",
+    headers=headers,
+    json={"login": username, "password": password},
+)
 
 if response.status_code == 200:
     token = response.json()["token"]
@@ -27,11 +20,76 @@ if response.status_code == 200:
 else:
     raise ValueError("Usuário ou senha inválidos")
 
-if responseCreate.status_code == 201:
-    data = responseCreate.json()
-    path = data["@id"]
-    print(f"Criado {path}")
-    url = f"{path}/@workflow/publish"
-    requests.post(url, headers=headers)
-else:
-    raise ValueError("Não Criado")
+# Create content
+
+def add_to_plone(path: str, payload: dict):
+    url = f"{base_url}{path}"
+    obj_id = payload["id"]
+    obj_url = f"{url}/{obj_id}"
+    response = requests.get(obj_url, headers=headers)
+    if response.status_code == 404:
+        # Content does not exist yet, create
+        response = requests.post(
+            url,
+            headers=headers,
+            json=payload,
+        )
+        if response.status_code > 299:
+            print(f"ERROR: {response.text}")
+            return
+        data = response.json()
+        path = data["@id"]
+        print(f"Created {path}")
+        url = f"{path}/@workflow/publish"
+        requests.post(url, headers=headers)
+    elif response.status_code in (200, 301, 302, 308):
+        # Content exists
+        data = response.json()
+        review_state = data.get("review_state")
+        obj_url = data["@id"]
+        if review_state == "private":
+            requests.post(f"{obj_url}/@workflow/publish", headers=headers)
+            print(f"Published {obj_url}")
+        response = requests.patch(
+            obj_url,
+            headers=headers,
+            json=payload,
+        )
+        print(f"Updated {obj_url}")
+    else:
+        # oops
+        print(f"ERROR: {url} {response.text}")
+
+
+# Create campus
+add_to_plone(
+    path="/",
+    payload={
+        "@type": "Document",
+        "id": "campus",
+        "title": "Campus",
+        "description": "Lista de Campus da UFT",
+    },
+)
+
+# Create campus palmas
+add_to_plone(
+    path="/campus",
+    payload={
+        "@type": "Document",
+        "id": "campus-palmas",
+        "title": "Campus Palmas",
+        "description": "Lista de Campus da UFT",
+    },
+)
+
+# Create campus araguaína
+add_to_plone(
+    path="/campus",
+    payload={
+        "@type": "Document",
+        "id": "campus-araguaina",
+        "title": "Campus Araguaína",
+        "description": "Lista de Campus da UFT",
+    },
+)
