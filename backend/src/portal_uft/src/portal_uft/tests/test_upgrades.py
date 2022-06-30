49fc693207408d409735c359a3f82c6be6c0965d
Paulo Rogerio <paulorogerio.to@gmail.com>
2022-06-30 14:53:29 -0300

    Exercício 51

--- backend/src/portal_uft/src/portal_uft/tests/test_upgrades.py
+++ backend/src/portal_uft/src/portal_uft/tests/test_upgrades.py
@@ -76,3 +76,11 @@ class V20220624001UpgradeTest(UpgradeStepIntegrationTest):
     src = "20220624001"
     dst = "20220629001"
     steps = 1
+
+
+class V20220629001UpgradeTest(UpgradeStepIntegrationTest):
+    """Test upgrade step from version 20220623001."""
+
+    src = "20220629001"
+    dst = "20220630001"
+    steps = 1
